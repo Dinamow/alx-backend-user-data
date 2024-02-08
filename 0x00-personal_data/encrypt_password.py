d13efb36772998542f22318a8d26d3717d0ee0d4
Dinamow <meemoo102039@gmail.com>
2024-02-08 20:35:58 +0200

    better design for Encrypting

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,16 +7,11 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """ Returns a salted, hashed password, which is a byte string """
-    encoded = password.encode()
-    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
-
-    return hashed
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Validates the provided password matches the hashed password """
-    valid = False
-    encoded = password.encode()
-    if bcrypt.checkpw(encoded, hashed_password):
-        valid = True
-    return valid
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    return False
