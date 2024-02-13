4bcbb122506cca7c44cebd608ab9281731e9f1a0
DINAMOW <meemoo102039@gmail.com>
2024-02-13 13:55:21 +0200

    fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,8 +10,8 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """extract base64 authorization header"""
-        if self.authorization_header(request) is None or\
-            not self.authorization_header(request).startswith('Basic ') or\
-                not isinstance(self.authorization_header(request), str):
+        if authorization_header is None or\
+            not isinstance(authorization_header, str)or\
+                not authorization_header.startswith('Basic '):
             return None
-        return self.authorization_header(request)[6:]
+        return authorization_header[6:]
