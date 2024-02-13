29abf7374ed82043fc85727586d6a08c08c6a735
DINAMOW <meemoo102039@gmail.com>
2024-02-13 13:37:04 +0200

    task7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """basic auth module"""
 from api.v1.auth.auth import Auth
+import base64
+from flask import request
 
 
 class BasicAuth(Auth):
     """basic auth class"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extract base64 authorization header"""
+        if self.authorization_header(request) is None or\
+            not self.authorization_header(request).startswith('Basic ') or\
+                not isinstance(self.authorization_header(request), str):
+            return None
+        return self.authorization_header(request)[6:]
