7fe59a52365856a00bd968c475d4f38f17fd1932
DINAMOW <meemoo102039@gmail.com>
2024-02-13 14:04:21 +0200

    task9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from api.v1.auth.auth import Auth
 import base64
 from flask import request
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -11,17 +12,30 @@ class BasicAuth(Auth):
                                             authorization_header: str) -> str:
         """extract base64 authorization header"""
         if authorization_header is None or\
-            not isinstance(authorization_header, str)or\
+            not isinstance(authorization_header, str) or\
                 not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
-    
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
         """decode base64 authorization header"""
-        if base64_authorization_header is None or\
-            not isinstance(base64_authorization_header, str):
+        b_None = base64_authorization_header is None
+        if b_None or not isinstance(base64_authorization_header, str):
             return None
         try:
-            return base64.b64decode(base64_authorization_header).decode('utf-8')
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> Tuple[str, str]:
+        """extract user credentials"""
+        if decoded_base64_authorization_header is None or\
+            not isinstance(decoded_base64_authorization_header, str) or\
+                ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':', 1))
