555d8e181c86fc8ec3cfb3d15e31be07ff20a80e
DINAMOW <meemoo102039@gmail.com>
2024-02-13 12:18:35 +0200

    fix again\

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,34 +7,34 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-	""" GET /api/v1/status
-	Return:
-		- the status of the API
-	"""
-	return jsonify({"status": "OK"})
+    """ GET /api/v1/status
+    Return:
+        - the status of the API
+    """
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> str:
-	""" unauthorized
-	"""
-	abort(401)
+    """ unauthorized
+    """
+    abort(401)
 
 
 @app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> str:
-	""" forbidden
-	"""
-	abort(403)
+    """ forbidden
+    """
+    abort(403)
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-	""" GET /api/v1/stats
-	Return:
-		- the number of each objects
-	"""
-	from models.user import User
-	stats = {}
-	stats['users'] = User.count()
-	return jsonify(stats)
+    """ GET /api/v1/stats
+    Return:
+        - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
