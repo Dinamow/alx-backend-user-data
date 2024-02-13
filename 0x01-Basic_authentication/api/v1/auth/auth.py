81f6e483eff779f6e7eeb9222b45ce9c2f234581
DINAMOW <meemoo102039@gmail.com>
2024-02-13 13:07:15 +0200

    task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,12 +17,15 @@ class Auth:
             return False
         for p in excluded_paths:
             if p[:-1] == path:
+                print(p[:-1])
                 return False
         return True
 
     def authorization_header(self, request=None) -> str:
         """authorization_header"""
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> User:
         """current_user"""
