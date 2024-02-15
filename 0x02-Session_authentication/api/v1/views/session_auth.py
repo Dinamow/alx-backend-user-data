102fe35511bf80b1461b3e0f51c0cdc4da9f0671
DINAMOW <meemoo102039@gmail.com>
2024-02-15 12:29:06 +0200

    change the code structure

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -17,18 +17,15 @@ def auth_session() -> str:
         return jsonify({"error": "email missing"}), 400
     if password is None or password == "":
         return jsonify({"error": "password missing"}), 400
-    
-    users = User.search({'email': email})
 
-    if not users:
+    if not User.search({'email': email}):
         return jsonify({"error": "no user found for this email"}), 404
-    
-    for user in users:
-        if not user.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
+
+    for user in User.search({'email': email}):
         from api.v1.app import auth
-        session_id = auth.create_session(user.id)
-        response = jsonify(user.to_json())
-        response.set_cookie('session_id', session_id)
-        return response
-    
+        if user.is_valid_password(password):
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
+            response.set_cookie('session_id', session_id)
+            return response
+    return jsonify({"error": "wrong password"}), 401
