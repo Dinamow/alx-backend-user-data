b91cb932925fd7e2cb956d077e257f97ae92b741
DINAMOW <meemoo102039@gmail.com>
2024-02-20 10:24:31 +0200

    uptodate

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -23,7 +23,7 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
-    
+
     def current_user(self, request=None) -> User:
         """Returns a User instance based on a cookie value"""
         session_id = self.session_cookie(request)
