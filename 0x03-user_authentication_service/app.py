9ceda90c547cd0aaa0d22a263d1b43c91df6ee09
DINAMOW <meemoo102039@gmail.com>
2024-02-21 11:03:10 +0200

    handle if not session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,7 +52,12 @@ def logout():
     """the logout route
     """
     session_id = request.cookies.get("session_id")
+
+    if not session_id:
+        abort(403)
+
     user = AUTH.get_user_from_session_id(session_id)
+
     if user:
         AUTH.destroy_session(user.id)
         return redirect('/')
