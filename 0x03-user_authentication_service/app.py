c3028ff97caf5a7d05d6786a20d870dec683785f
DINAMOW <meemoo102039@gmail.com>
2024-02-21 10:46:25 +0200

    adding login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -30,6 +30,19 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """the login route
+    """
+    if AUTH.valid_login(request.form.get('email'), request.form.get('password')):
+        session_id = AUTH.create_session(request.form.get('email'))
+        response = jsonify({"email": request.form.get('email'), "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
